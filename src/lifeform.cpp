d20d0d7491841d02c21903b6be30d64fff649e9a
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2015-09-06 17:43:42 +0300

    minor beautification

--- src/lifeform.cpp
+++ src/lifeform.cpp
@@ -9,17 +9,13 @@ LifeForm::LifeForm(double pos_x, double pos_y)
 
 void LifeForm::update(uint32_t elapsed)
 {
-    m_pos_x += 0.01 * elapsed;
-    m_pos_y += 0.01 * elapsed;
+    m_pos_x += 0.03 * elapsed;
+    m_pos_y += 0.03 * elapsed;
 }
 
 void LifeForm::render(SDL_Renderer* renderer)
 {
-    SDL_Rect rect;
-    rect.x = m_pos_x;
-    rect.y = m_pos_y;
-    rect.h = 50;
-    rect.w = 50;
+    SDL_Rect rect {(int)round(m_pos_x), (int)round(m_pos_y), 8, 8};
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-    SDL_RenderDrawRect(renderer, &rect);
+    SDL_RenderFillRect(renderer, &rect);
 }
