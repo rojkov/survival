4587a4ebc58f59ebed19c35cb4da30cddf461873
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2015-09-20 15:26:03 +0300

    drop intermediate variables

--- src/world.cpp
+++ src/world.cpp
@@ -55,10 +55,10 @@ World::World(std::shared_ptr<SDL_Renderer> renderer, const Rect &viewport_rect)
     });
 
     // Create world texture
-    Size size = m_txt_rect.size;
     m_texture.reset(SDL_CreateTexture(m_renderer.get(), SDL_PIXELFORMAT_RGBA8888,
                                       SDL_TEXTUREACCESS_TARGET,
-                                      size.width, size.height));
+                                      m_txt_rect.size.width,
+                                      m_txt_rect.size.height));
     assert(m_texture != nullptr);
     refresh_texture(viewport_rect);
 }
@@ -105,19 +105,18 @@ void World::refresh_texture(const Rect &viewport)
                                   -1 * (viewport.offset.y % 16)))
                       .move_inside(Rect(0, 0, WORLD_WIDTH * 16, WORLD_HEIGHT * 16));
 
-    int offset_x = m_txt_rect.offset.x, offset_y = m_txt_rect.offset.y;
-
     for (int i = 0; i <= m_txt_rect.size.width/16; i++) {
-        int tile_x_pos = i + offset_x/16;
+        int tile_x_pos = i + m_txt_rect.offset.x/16;
         if (tile_x_pos >= WORLD_WIDTH || tile_x_pos < 0) {
             continue;
         }
         for (int j = 0; j <= m_txt_rect.size.height/16; j++) {
-            int tile_y_pos = j + offset_y/16;
+            int tile_y_pos = j + m_txt_rect.offset.y/16;
             if (tile_y_pos >= WORLD_HEIGHT || tile_y_pos < 0) {
                 continue;
             }
-            SDL_Rect rect {i * 16 - (offset_x % 16), j * 16 - (offset_y % 16), 16, 16};
+            SDL_Rect rect {i * 16 - (m_txt_rect.offset.x % 16),
+                           j * 16 - (m_txt_rect.offset.y % 16), 16, 16};
             SDL_RenderCopy(m_renderer.get(), m_tiles.at(tile_x_pos, tile_y_pos)->get_texture(),
                            nullptr, &rect);
         }
